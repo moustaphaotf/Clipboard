52a49a5fd198e07e806b8d1881abd24f924bdf73
Mamadou Moustapha Diallo <moustaphaotf@gmail.com>
2023-02-12 19:44:51 +0000

    Displaying the content of a note and the update form

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,13 +1,23 @@
+const sqlite = require('sqlite3');
+
 exports.notes_list = function (req, res, next) {
   res.send("NOT IMPLEMENTED: Liste des notes !");
 }
 
 exports.note_detail = function(req, res, next) {
-  res.send("NOT IMPLEMENTED: Détail d'une note: " + req.params.id);
+  const db = new sqlite.Database('model/clipboard.db', err => {
+    if(err) console.log("Erreur lors de l'ouverture de la base de données !");
+  });
+
+  db.get("SELECT * FROM notes WHERE id = ?", [req.params.id], (err, result) => {
+    if(err) next(err);
+    
+    res.render("note_details", {note_details: result});
+  }) 
 }
 
 exports.note_create_get = function (req, res, next) {
-  res.render('note_create', {title: "Une nouvelle note"})
+  res.render('note_upsert', {title: "Une nouvelle note"})
 }
 
 exports.note_create_post = function (req, res, next) {
@@ -16,11 +26,20 @@ exports.note_create_post = function (req, res, next) {
 }
 
 exports.note_update_get = function (req, res, next) {
-  res.send("NOT IMPLEMENTED: Mise à jour d'une note get!");
+  const db = new sqlite.Database('model/clipboard.db', err => {
+    if(err) console.log("Erreur lors de l'ouverture de la base de données !");
+  });
+
+  db.get("SELECT * FROM notes WHERE id = ?", [req.params.id], (err, result) => {
+    if(err) next(err);
+    
+    res.render("note_upsert", {title:"Modifier une note", note_details: result});
+  });
 }
 
 exports.note_update_post = function (req, res, next) {
   res.send("NOT IMPLEMENTED: Mise à jour d'une note post!");
+  console.log(req.body)
 }
 
 exports.note_delete_get = function (req, res, next) {
